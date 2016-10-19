0292640578fb4240bb8d86c7601289d01cd49c21
Devarsh14 <Devarsh14@gmail.com>
2016-10-20 02:08:34 +1000

    Add project files.

---
+++ AzureContactManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureContactManager.Startup))]
+namespace AzureContactManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
