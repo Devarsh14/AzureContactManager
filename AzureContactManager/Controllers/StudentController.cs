ad719437ac93076bc901e28d9390d40abb4db50f
Devarsh14 <Devarsh14@gmail.com>
2016-10-25 19:49:45 +1000

    Connected with Database on Azure Created first time Entity framework model Added connection string with model Sql Azure database connected with local DB Alll files from local DB transfferd to AZure

---
+++ AzureContactManager/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AzureContactManager.Models;
+using System.Web.Mvc;
+
+namespace AzureContactManager.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var stud = new Student();
+            stud.FirstName = "Devarsh";
+            stud.LastName = "shukla";
+            stud.StudentNo = 12;
+
+            return View(stud);
+        }
+    }
+}
