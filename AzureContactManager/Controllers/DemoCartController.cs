94623a6abb7bc75a254b7c35c384b49e9edea303
Devarsh <Devarsh14@gmail.com>
2016-11-19 23:00:10 +0000

    first list populated.

---
+++ AzureContactManager/Controllers/DemoCartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AzureContactManager.Models;
+
+namespace AzureContactManager.Controllers
+{
+    public class DemoCartController : Controller
+    {
+        // GET: DemoCart
+        AzureContactManagerDev_dbEntities2 db = new AzureContactManagerDev_dbEntities2();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ShowProduct()
+        {
+            
+            return View(db.OrderItems.ToList());
+            
+        }
+    }
+}
