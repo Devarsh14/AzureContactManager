2f0744ad61e075c66d5a2f559ac3d4876e638108
Devarsh14 <Devarsh14@gmail.com>
2016-11-21 20:16:09 +1000

    new class model Cartshop model added

---
+++ AzureContactManager/Models/CartShopModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureContactManager.Models
+{
+    public class CartShopModel
+    {
+       
+    }
+    public class Cart
+    {
+        List<ShoppingCartItem> items = new List<ShoppingCartItem>();
+        
+        
+        public IEnumerable<ShoppingCartItem> Items
+        {
+            get { return items; }
+        }
+
+        //methods 
+
+    }
+}
